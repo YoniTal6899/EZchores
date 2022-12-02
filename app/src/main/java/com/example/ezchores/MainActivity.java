58d055b76b66327aa62564d7e4188255663cd0f2
gars-cmd <avidouille@gmail.com>
2022-12-02 12:48:02 +0200

    add classes : Task , User ,Auth_utils , group

--- app/src/main/java/com/example/ezchores/MainActivity.java
+++ app/src/main/java/com/example/ezchores/MainActivity.java
@@ -1,41 +1,76 @@
 package com.example.ezchores;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    private Button login,signup;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        login=(Button) findViewById(R.id.log_in_button);
-        signup=(Button) findViewById(R.id.sign_up_button);
+  private Button login, signup;
+  private FirebaseAuth mAuth;
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                open_LogIn_Activity();
-            }
-        });
+  FirebaseDatabase database = FirebaseDatabase.getInstance();
+  DatabaseReference myRef = database.getReference("message");
 
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                open_SignUp_Activity();
-            }
-        });
-    }
-    public void open_LogIn_Activity(){
-        Intent home_to_login= new Intent(this,LogIn_Activity.class);
-        startActivity(home_to_login);
-    }
-    public void open_SignUp_Activity(){
-        Intent home_to_signup= new Intent(this,SignUp_Activity.class);
-        startActivity(home_to_signup);
-    }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    login = (Button)findViewById(R.id.log_in_button);
+    signup = (Button)findViewById(R.id.sign_up_button);
+    mAuth = FirebaseAuth.getInstance();
+
+    login.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        open_LogIn_Activity();
+      }
+
+    });
+
+
+
+    signup.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        open_SignUp_Activity();
+      }
+    });
+  }
+
+  public void open_LogIn_Activity() {
+    Intent home_to_login = new Intent(this, LogIn_Activity.class);
+    startActivity(home_to_login);
+  }
+
+  public void open_SignUp_Activity() {
+    Intent home_to_signup = new Intent(this, SignUp_Activity.class);
+    startActivity(home_to_signup);
+  }
+
+//  @Override
+//  public void onStart() {
+//    super.onStart();
+//    // Check if user is signed in (non-null) and update UI accordingly.
+//    FirebaseUser currentUser = mAuth.getCurrentUser();
+//    if(currentUser != null){
+//      currentUser.reload();
+//    }else{
+//      startActivity(new Intent(MainActivity.this , LogIn_Activity.class));
+//    }
+//  }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/ezchores/User.java
@@ -0,0 +1,47 @@
+package com.example.ezchores;
+
+import java.util.ArrayList;
+
+public class User {
+    int id;
+    String name;
+    ArrayList<Group> groupList;
+    ArrayList<Task> taskList;
+
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Group> getGroupList() {
+        return groupList;
+    }
+
+    public void setGroupList(ArrayList<Group> groupList) {
+        this.groupList = groupList;
+    }
+
+    public ArrayList<Task> getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(ArrayList<Task> taskList) {
+        this.taskList = taskList;
+    }
+}
