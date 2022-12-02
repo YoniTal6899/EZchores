781197240b1c74e0f2c46bf5f795636aca91d01f
gars-cmd <avidouille@gmail.com>
2022-12-02 13:54:19 +0200

    add comments and clean up a little bit , fix some email compatibilities and some password valid issues

--- app/src/main/java/com/example/ezchores/LogIn_Activity.java
+++ app/src/main/java/com/example/ezchores/LogIn_Activity.java
@@ -24,9 +24,14 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LogIn_Activity extends AppCompatActivity {
+
+    // Buttons
     Button back, commit_login;
 
+    //fields
     private AppCompatEditText mail_field , password_field ;
+
+    // Firebase
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
@@ -35,20 +40,25 @@ public class LogIn_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        // Buttons
+
+        // Buttons inits
         mail_field = findViewById(R.id.Email_field);
         password_field = findViewById(R.id.Password_field);
         back=findViewById(R.id.back_home);
         commit_login=findViewById(R.id.commit_login);
 
-
+        // Firebase init
         mAuth = FirebaseAuth.getInstance();
+
+        // back button listener
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 back_home();
             }
         });
+
+        //  commmit button listener
         commit_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,10 +67,16 @@ public class LogIn_Activity extends AppCompatActivity {
         });
     }
 
+    // login function
     private void loginUser() {
+
+        //get the text from the fields
         String email = mail_field.getText().toString();
         String password = password_field.getText().toString();
+
+        // checker object for validate infos
         Auth_utils checker = new Auth_utils();
+
 
         if (checker.password_validity(password) == false) {
             password_field.setError("The password need to contain at least 8 characters including  : one digit , one Uppercase , one low case , one special character ");
@@ -72,23 +88,20 @@ public class LogIn_Activity extends AppCompatActivity {
             mail_field.setError("you need to enter a valid email");
             mail_field.requestFocus();
         } else {
-           mAuth.signInWithEmailAndPassword(email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-               @Override
-               public void onComplete(@NonNull Task<AuthResult> task) {
+            mAuth.signInWithEmailAndPassword(email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
                         Toast.makeText(LogIn_Activity.this , "User logged in successfully" , Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(LogIn_Activity.this , My_Groups_Activity.class));
                     }else {
                         Toast.makeText(LogIn_Activity.this ,
-                                        task.getException().getMessage()
-                                        ,
+                                task.getException().getMessage(),
                                 Toast.LENGTH_SHORT).show();
                     }
-
-               }
-           });
+                }
+            });
         }
-
     }
 
     public void back_home(){
--- app/src/main/java/com/example/ezchores/User.java
+++ app/src/main/java/com/example/ezchores/User.java
@@ -3,19 +3,19 @@ package com.example.ezchores;
 import java.util.ArrayList;
 
 public class User {
-    int id;
-    String name;
-    ArrayList<Group> groupList;
-    ArrayList<Task> taskList;
+    // parameters
+   private  int id;
+   private  String name;
+   private  ArrayList<Group> groupList;
+   private  ArrayList<Task> taskList;
 
 
+    // constructor
     public User(String name) {
         this.name = name;
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() {return id;}
 
     public void setId(int id) {
         this.id = id;
