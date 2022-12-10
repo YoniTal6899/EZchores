6357f8eaa6137b342718106a4b3ad105e8316f99
Ofek Rotem <92383710+ofekrotem@users.noreply.github.com>
2022-12-10 15:06:10 +0200

    Add group creator to friends in DB + remove unnecessary imports

--- app/src/main/java/com/example/ezchores/Pending_Items_Activity.java
+++ app/src/main/java/com/example/ezchores/Pending_Items_Activity.java
@@ -1,8 +1,6 @@
 package com.example.ezchores;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
